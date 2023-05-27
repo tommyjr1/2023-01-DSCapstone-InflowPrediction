d3e300b1669f4eb544e03f7949f0ef6959341105
Jaehyun Song <tommyjr1@naver.com>
2023-05-27 15:53:58 +0900

    fix/encoding + add/automl

--- code/data/utils/damlib.py
+++ code/data/utils/damlib.py
@@ -109,7 +109,7 @@ def collect(startY, startM, startD, endY, endM, endD, serviceKey, url):
         df2 = pd.DataFrame(row)
         df2 = df2[df2["댐이름"] == "합천"]
         df2.to_csv("./data/합천다목적댐/합천다목적댐_" +
-                   vdate.strftime("%Y-%m-%d")+".csv", index=False)
+                   vdate.strftime("%Y-%m-%d")+".csv", index=False, encoding="utf-8-sig" )
         date = date+dt.timedelta(days=1)
 
 
@@ -118,7 +118,7 @@ def concat(startY, endY):
 
     total = pd.DataFrame()
     for file_name in file_names:
-        temp = pd.read_csv(file_name, encoding='utf-8', low_memory=False)
+        temp = pd.read_csv(file_name, low_memory=False, encoding="utf-8-sig" )
         temp = temp[temp["댐이름"] == "합천"]
         total = pd.concat([total, temp], ignore_index=True)
 
@@ -146,7 +146,7 @@ def concat(startY, endY):
 
         total.loc[(total.index.to_pydatetime() >= start_date) & (
             total.index.to_pydatetime() < end_date), '홍수기'] = 1
-    total.to_csv(f"./data/합천다목적댐_전체.csv")
+    total.to_csv(f"./data/합천다목적댐_전체.csv", encoding="utf-8-sig" )
 
     total_daily = total.resample(rule='D').mean()
-    total_daily.to_csv(f"./data/합천다목적댐_전체_일별.csv")
+    total_daily.to_csv(f"./data/합천다목적댐_전체_일별.csv", encoding="utf-8-sig" )
--- code/data/utils/preprocesslib.py
+++ code/data/utils/preprocesslib.py
@@ -4,14 +4,16 @@ import os
 
 
 def merge(dam_file, weather_file, output_name):
-    dam = pd.read_csv(f'./data/{dam_file}.csv', index_col='시간')
+    dam = pd.read_csv(f'./data/{dam_file}.csv',
+                      index_col='시간', encoding="utf-8-sig")
     dam.index = pd.to_datetime(dam.index)
 
-    weather = pd.read_csv(f'./data/{weather_file}.csv', index_col='일시')
+    weather = pd.read_csv(
+        f'./data/{weather_file}.csv', index_col='일시', encoding="utf-8-sig")
     weather.index = pd.to_datetime(weather.index)
     df = pd.concat([dam, weather], axis=1)
     df.dropna(inplace=True)
-    df.to_csv(f'./data/{output_name}.csv')
+    df.to_csv(f'./data/{output_name}.csv', encoding="utf-8-sig")
 
 
 def generate_cyclical_features(df, col_name, period, start_num=0):
@@ -23,16 +25,16 @@ def generate_cyclical_features(df, col_name, period, start_num=0):
 
 
 def preprocessDam(file_name):
-    data = pd.read_csv(f'./data/{file_name}.csv')
+    data = pd.read_csv(f'./data/{file_name}.csv',
+                       encoding="utf-8-sig", index_col='시간')
+    data.index = pd.to_datetime(data.index)
+
     # data['1일후유입량'] = data['당일유입량'][1:].reset_index()['당일유입량']
     # data['2일후유입량'] = data['당일유입량'][2:].reset_index()['당일유입량']
 
-    data = data[['시간', '저수량(현재)', '전일방류량(본댐)',
+    data = data[['저수량(현재)', '전일방류량(본댐)',
                  '당일유입량',  '홍수기']]
     # data = data.iloc[:-2,]
-
-    data.set_index('시간')
-    data.index = pd.to_datetime(data.index)
 
     df_date = data.assign(month=data.index.month).assign(
         day_of_week=data.index.dayofweek).assign(week_of_year=data.index.isocalendar().week)
@@ -40,15 +42,16 @@ def preprocessDam(file_name):
     df_date = generate_cyclical_features(df_date, 'month', 12, 1)
     df_date = generate_cyclical_features(df_date, 'week_of_year', 52, 0)
 
-    df_date.to_csv(f'./data/{file_name}_forTrain.csv', index=False)
+    df_date.to_csv(f'./data/{file_name}_forTrain.csv', encoding="utf-8-sig")
 
 
 def preprocessWeather(file_name):
-    data = pd.read_csv(f'./data/{file_name}.csv')
+    data = pd.read_csv(f'./data/{file_name}.csv', encoding="utf-8-sig")
     data['1일후강수량'] = data['강수량(mm)'][1:].reset_index()['강수량(mm)']
     data['2일후강수량'] = data['강수량(mm)'][2:].reset_index()['강수량(mm)']
     data = data[['일시', '기온(°C)', '강수량(mm)', '지면온도(°C)',
                  '습도(%)', '1일후강수량', '2일후강수량']]
     data = data.iloc[:-2,]
 
-    data.to_csv(f'./data/{file_name}_forTrain.csv', index=False)
+    data.to_csv(f'./data/{file_name}_forTrain.csv',
+                index=False, encoding="utf-8-sig")
